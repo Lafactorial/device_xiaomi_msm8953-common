515f85557bde3648c16f29450057dd64022051ad
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    msm8953-common: Removed audio A2dp
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- msm8953.mk
+++ msm8953.mk
@@ -85,7 +85,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
