windbell-project/android_device_xiaomi_msm8953-common

msm8953-common: Implement extension for kernelspace battery saver  * this activates kernelspace battery saver via powerhal whenever    battery saver is enabled in userspace, thereby lowering power    consumption at kernel-level by disabling boosts and such

BoardConfigCommon.mk
@@ -126,6 +126,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true

Lafactorial/device_xiaomi_msm8953-common

BoardConfigCommon.mk
@@ -111,6 +111,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true