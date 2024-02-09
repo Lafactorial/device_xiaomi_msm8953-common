b1b25ebef420614e2c49f27772cd9eb0384bca7c
Hakan <hknhzl@gmail.com>
2024-02-09 07:11:18 +0300

    Update remove packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CalendarGooglePrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt
-LOCAL_OVERRIDES_PACKAGES += PhotoTable
+LOCAL_OVERRIDES_PACKAGES := CalendarGooglePrebuilt CalculatorGooglePrebuilt Chrome
+LOCAL_OVERRIDES_PACKAGES += FilesPrebuilt SafetyHubPrebuilt
+LOCAL_OVERRIDES_PACKAGES += PhotoTable talkback Velvet
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
