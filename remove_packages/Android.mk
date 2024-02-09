2324f0a171fcfb876492290becea51dc5c17d900
Hakan <hknhzl@gmail.com>
2024-02-09 19:27:16 +0300

    Romove more packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CalendarGooglePrebuilt CalculatorGooglePrebuilt Chrome
-LOCAL_OVERRIDES_PACKAGES += FilesPrebuilt SafetyHubPrebuilt
-LOCAL_OVERRIDES_PACKAGES += PhotoTable talkback Velvet
+LOCAL_OVERRIDES_PACKAGES := AppDirectedSMSService CalendarGooglePrebuilt CalculatorGooglePrebuilt Chrome
+LOCAL_OVERRIDES_PACKAGES += Chrome-Stub FilesPrebuilt GoogleTTS KidsSupervisionStub MaestroPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt PhotoTable talkback Velvet
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
