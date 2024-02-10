560e52c7552c9fe305f93b31d68137167eaf5171
Hakan <hknhzl@gmail.com>
2024-02-10 21:48:32 +0300

    msm8953-common : Remove more packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX GoogleTTS Photos PixelWallpapers2021 talkback SafetyHubPrebuilt WellbeingPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AppDirectedSMSService CalendarGooglePrebuilt CalculatorGooglePrebuilt
+LOCAL_OVERRIDES_PACKAGES += Chrome FilesPrebuilt GoogleTTS
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt PixelWallpapers2021 PhotoTable talkback Velvet
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
