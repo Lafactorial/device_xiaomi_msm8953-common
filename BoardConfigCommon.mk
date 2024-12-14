364596471ce63be2ab1b159d21042a8c2f11c6ce
Giovanni Ricca <giovanniricca@proton.me>
2024-12-14 13:01:55 +0100

    msm8953-common: rootdir: `Android.mk` -> `Android.bp`
    * Also decommonize fstab as it would become a mess to maintain :D.
    
    Change-Id: I1f1885af7d84c1be01dd257d99b43a84f4be49f6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,17 +125,6 @@ TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Recovery
-ifeq ($(AB_OTA_UPDATER), true)
-    ifeq ($(TARGET_IS_LEGACY), true)
-        TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab_legacy_AB.qcom
-    else
-        TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab_AB.qcom
-    endif
-else ifeq ($(TARGET_IS_LEGACY), true)
-    TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab_legacy.qcom
-else
-    TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
-endif
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USERIMAGES_USE_EXT4 := true
 
